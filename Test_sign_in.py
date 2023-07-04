8b2a62f6fb5ba5fa625c0c752808650ab984cb64
PervykhNE <nat.pervykh@gmail.com>
2023-07-04 22:57:53 +0300

    Test

--- Test_sign_in.py
+++ Test_sign_in.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
@@ -8,7 +9,12 @@ username = 'standard_user'
 password = 'secret_sauce'
 
 def get_driver():
-    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+    chrome_options = Options()
+    chrome_options.add_argument("--window-size=1920,800")
+    chrome_options.add_argument("--headless")
+    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()),
+                              options=chrome_options)
+    driver.implicitly_wait(10)
     return driver
 
 def open_page(driver, url):
